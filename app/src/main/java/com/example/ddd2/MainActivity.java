e49f7988b45b86b1db1369950d7b5e103a0d9dad
LolKia <remeyztoor@gmail.com>
2024-02-08 14:52:45 +0900

    8 feb

---
+++ app/src/main/java/com/example/ddd2/MainActivity.java
@@ -0,0 +1,28 @@
+package com.example.ddd2;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+    private TextView myTextVar;
+    private CheckBox chkBox;
+    private Button btn;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main4);
+        myTextVar = findViewById(R.id.myText);
+        chkBox = findViewById(R.id.checkBox);
+        btn = findViewById(R.id.button21);
+
+        myTextVar.setText("New text in TextView");
+        chkBox.setChecked(true);
+        btn.setEnabled(false);
+    }
+}
